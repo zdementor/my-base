4e3dda6a6b0d9d8e28a1d39104dcbb727cef69b4
dzhuk <zdimitor@gmail.com>
2016-02-16 17:12:34 +0300

    Compilation scripts fixes

--- src/scr/tolua++.cmd
+++ src/scr/tolua++.cmd
@@ -1,4 +1,4 @@
 cd package
 echo Binding lua...
-tolua++.exe -n MyLuaBind -o ../MyLuaBind.cpp -H ../MyLuaBind.h my.pkg
+..\..\..\..\bin\tolua++.exe -n MyLuaBind -o ../MyLuaBind.cpp -H ../MyLuaBind.h my.pkg
 pause
